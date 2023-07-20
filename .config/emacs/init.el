d1b6f55418c38dc23167b45f541d7b131d4ae531
Suzuki Aki <suzuki11109@gmail.com>
2023-07-20 22:49:59 +0300

    update magit config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -474,8 +474,7 @@ If FOREVER is non-nil, the file is deleted without being moved to trash."
   :straight (:type built-in)
   :hook
   ((prog-mode text-mode) . electric-pair-mode)
-  :hook ((git-commit-mode . git-commit-add-electric-pairs)
-         (markdown-mode . markdown-add-electric-pairs)
+  :hook ((markdown-mode . markdown-add-electric-pairs)
          (go-ts-mode . go-add-electric-pairs)
          (yaml-ts-mode . yaml-add-electric-pairs))
   :hook
@@ -484,9 +483,6 @@ If FOREVER is non-nil, the file is deleted without being moved to trash."
                             `(lambda (c)
                                (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
   :preface
-  (defun git-commit-add-electric-pairs ()
-    (setq-local electric-pair-pairs (append electric-pair-pairs '((?` . ?`) (?= . ?=))))
-    (setq-local electric-pair-text-pairs electric-pair-pairs))
   (defun markdown-add-electric-pairs ()
     (setq-local electric-pair-pairs (append electric-pair-pairs '((?` . ?`))))
     (setq-local electric-pair-text-pairs electric-pair-pairs))
@@ -1030,6 +1026,15 @@ If FOREVER is non-nil, the file is deleted without being moved to trash."
   (rfn-eshadow-update-overlay . vertico-directory-tidy)
   (minibuffer-setup . vertico-repeat-save))
 
+(use-package git-commit
+  :after magit
+  :custom
+  (git-commit-summary-max-length 72) ;; defaults to Github's max commit message length
+  (git-commit-style-convention-checks '(overlong-summary-line non-empty-second-line))
+  :config
+  (evil-set-initial-state 'git-commit-mode 'insert)
+  (global-git-commit-mode 1))
+
 (use-package magit
   :general
   (+leader-def :infix "g"
@@ -1044,9 +1049,85 @@ If FOREVER is non-nil, the file is deleted without being moved to trash."
   (magit-diff-refine-hunk t)
   (magit-revision-show-gravatars t)
   (magit-save-repository-buffers nil)
-  (magit-display-buffer-function #'magit-display-buffer-fullcolumn-most-v1)
+  ;; (magit-display-buffer-function #'magit-display-buffer-fullcolumn-most-v1)
+  (magit-revision-insert-related-refs nil)
+  (magit-bury-buffer-function #'magit-mode-quit-window)
+
   :config
-  (add-hook 'git-commit-mode-hook 'evil-insert-state)
+  (add-hook 'magit-process-mode-hook #'goto-address-mode)
+  (add-hook 'magit-popup-mode-hook #'hide-mode-line-mode)
+
+  ;; Close transient with ESC
+  (define-key transient-map [escape] #'transient-quit-one)
+
+  (defun +magit-display-buffer-fn (buffer)
+    "Same as `magit-display-buffer-traditional', except...
+
+- If opened from a commit window, it will open below it.
+- Magit process windows are always opened in small windows below the current.
+- Everything else will reuse the same window."
+    (let ((buffer-mode (buffer-local-value 'major-mode buffer)))
+      (display-buffer
+       buffer (cond
+               ((and (eq buffer-mode 'magit-status-mode)
+                     (get-buffer-window buffer))
+                '(display-buffer-reuse-window))
+               ;; Any magit buffers opened from a commit window should open below
+               ;; it. Also open magit process windows below.
+               ((or (bound-and-true-p git-commit-mode)
+                    (eq buffer-mode 'magit-process-mode))
+                (let ((size (if (eq buffer-mode 'magit-process-mode)
+                                0.35
+                              0.7)))
+                  `(display-buffer-below-selected
+                    . ((window-height . ,(truncate (* (window-height) size)))))))
+
+               ;; Everything else should reuse the current window.
+               ((or (not (derived-mode-p 'magit-mode))
+                    (not (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode))))
+                '(display-buffer-same-window))
+
+               ('(+magit--display-buffer-in-direction))))))
+
+  (defun +magit--display-buffer-in-direction (buffer alist)
+    "`display-buffer-alist' handler that opens BUFFER in a direction.
+
+This differs from `display-buffer-in-direction' in one way: it will try to use a
+window that already exists in that direction. It will split otherwise."
+    (let ((direction (or (alist-get 'direction alist)
+                         +magit-open-windows-in-direction))
+          (origin-window (selected-window)))
+      (if-let (window (window-in-direction direction))
+          (unless magit-display-buffer-noselect
+            (select-window window))
+        (if-let (window (and (not (one-window-p))
+                             (window-in-direction
+                              (pcase direction
+                                (`right 'left)
+                                (`left 'right)
+                                ((or `up `above) 'down)
+                                ((or `down `below) 'up)))))
+            (unless magit-display-buffer-noselect
+              (select-window window))
+          (let ((window (split-window nil nil direction)))
+            (when (and (not magit-display-buffer-noselect)
+                       (memq direction '(right down below)))
+              (select-window window))
+            (display-buffer-record-window 'reuse window buffer)
+            (set-window-buffer window buffer)
+            (set-window-parameter window 'quit-restore (list 'window 'window origin-window buffer))
+            (set-window-prev-buffers window nil))))
+      (unless magit-display-buffer-noselect
+        (switch-to-buffer buffer t t)
+        (selected-window))))
+
+  (setq magit-display-buffer-function #'+magit-display-buffer-fn)
+
   ;; for dotfiles
   (defun +magit-process-environment (env)
     "Add GIT_DIR and GIT_WORK_TREE to ENV when in a special directory.
