3182dfdc2f8c110a0782752a5d1bdf8817e06d04
Suzuki Aki <suzuki11109@gmail.com>
2023-07-23 21:42:04 +0300

    update config

--- .dwm/dwmblocks-config.h
+++
@@ -1,9 +0,0 @@
-#define CMDLENGTH 60
-#define DELIMITER ""
-#define CLICKABLE_BLOCKS
-
-const Block blocks[] = {
-	BLOCK("sb-volume",  0,    10),
-	BLOCK("sb-battery", 30,   23),
-	BLOCK("sb-date",    1,    24)
-};
