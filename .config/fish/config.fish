516e343a3eb0172cdb36859792d87b735acb5574
Suzuki Aki <suzuki11109@gmail.com>
2023-10-05 22:58:54 +0300

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias vi='nvim'
+    alias vim='nvim'
+    alias k='kubectl'
+    alias dot='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+end
