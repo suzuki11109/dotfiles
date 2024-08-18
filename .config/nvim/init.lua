99f77aabbfc02a3198f08c5df4744ec2049ed674
Aki Suzuki <aki@Akis-MacBook-Pro.local>
2024-08-18 15:15:39 +0700

    clean nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,41 +1,62 @@
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system {
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable',
-      lazypath,
-   }
+function map(mode, keys, func, opts)
+   local options = { noremap = true, silent = true }
+   if opts then
+      options = vim.tbl_extend('force', options, opts)
+   end
+
+   vim.keymap.set(mode, keys, func, opts)
 end
 
+-- treesitter
+-- comment
+-- noswap file
+-- surround/autopairs
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
 vim.opt.rtp:prepend(lazypath)
 
-require 'config.options'
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
-if os.getenv 'NVIM' ~= nil then
-   require('lazy').setup {
-      { 'willothy/flatten.nvim', config = true },
-   }
-   return
-end
-
-require('lazy').setup('plugins', {
-   defaults = {
-      lazy = true,
-   },
-   ui = {
-      border = 'rounded',
-   },
-   change_detection = {
-      notify = false,
-   },
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  },
+  checker = { enabled = true },
 })
 
-require 'config.mappings'
+vim.cmd.colorscheme "catppuccin"
 
-vim.cmd [[set statusline=%!v:lua.require'statusline'.output()]]
-vim.cmd 'colorscheme catppuccin'
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.termguicolors = true
+vim.opt.list = true
+vim.opt.listchars = "tab:  ,trail:·,eol: ,nbsp:_"
+vim.opt.cmdheight = 1
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
 
--- vim: ts=3 sts=3 sw=3 et
+-- [[ Basic Keymaps ]]
+map('', ';', ':')
+
