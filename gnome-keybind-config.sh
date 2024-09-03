0f44a691d8b6f45700766f3140a343add8f9238d
Aki Suzuki <suzuki11109@gmail.com>
2024-09-03 23:47:19 +0700

    delete unused gnome keybindings

--- gnome-keybind-config.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..6}; do 
-  gsettings set "org.gnome.shell.keybindings" "switch-to-application-${i}" "[]"
-  gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-${i}" "['<Super>${i}']"
-  gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-${i}" "['<Super><Shift>${i}']"
-done
