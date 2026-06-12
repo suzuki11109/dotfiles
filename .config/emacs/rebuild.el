18cca04c96e34a27c95f2bf5fcd0a8407138223c
Aki Suzuki <suzuki11109@gmail.com>
2026-06-13 04:11:44 +0700

    fix update emacs packages script

--- .config/emacs/rebuild.el
+++ .config/emacs/rebuild.el
@@ -2,30 +2,14 @@
 
 (load "~/.config/emacs/init.el")
 
-(message "Processing Elpaca queues...")
-(elpaca-process-queues)
-(elpaca-wait)
+(package-refresh-contents)
+(package-upgrade-all)
 
-(when (fboundp 'elpaca-update-all)
-  (message "Updating packages...")
-  (elpaca-update-all)
-  (elpaca-process-queues)
-  (elpaca-wait))
+(let ((files
+       (directory-files-recursively
+        package-user-dir
+        "\\.el\\'")))
+  (dolist (file files)
+    (native-compile file)))
 
-(let ((builds-dir
-       (expand-file-name "elpaca/builds"
-                         user-emacs-directory)))
-
-  (message "Compiling packages from %s" builds-dir)
-
-  (dolist (file
-           (directory-files-recursively
-            builds-dir
-            "\\.el\\'"))
-    (condition-case err
-        (native-compile file)
-      (error
-       (message "Failed %s: %S" file err)))))
-
-(message "Finished.")
 (kill-emacs 0)
